75163c6d4a54e44a224f65abfc26ae121eb30e38
juan poveda <poved4@gmail.com>
2025-05-10 10:33:02 -0500

    configurar enrutador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SignInComponent,
+    title: 'Supermercados Globales',
+  }
+];
