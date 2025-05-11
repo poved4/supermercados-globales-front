31df79881e97298f34405f96a2f94fea21ccf77d
juan poveda <poved4@gmail.com>
2025-05-10 23:29:46 -0500

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: SignInComponent,
     title: 'Supermercados Globales',
+  },
+  {
+    path: 'signUp',
+    component: SignUpComponent,
+    title: 'Supermercados Globales',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
