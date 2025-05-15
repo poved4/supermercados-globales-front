72ba4f0cb911d1c5261ce8c717f1579007757f66
juan poveda <poved4@gmail.com>
2025-05-14 19:56:58 -0500

    http request and routing service configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,17 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: SignInComponent,
-    title: 'Supermercados Globales',
   },
   {
     path: 'signUp',
     component: SignUpComponent,
-    title: 'Supermercados Globales',
   },
   {
     path: 'dashboard',
