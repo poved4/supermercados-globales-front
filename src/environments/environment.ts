ca2b4727e52028ac1dada1e3567a7db7facbd427
juan poveda <poved4@gmail.com>
2025-05-14 19:53:19 -0500

    environment variables file added

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+
+  production: false,
+
+  apiUrl: 'http://microservice-1095523925810.us-central1.run.app',
+
+  storage: {
+    accessToken: 'SG_ACCESS_TOKEN',
+    brach: 'SG_BRANCHES',
+    product: 'SG_PRODUCT',
+    order: 'SG_ORDER',
+    orderStatus: 'SG_ORDER_STATUS',
+    inventory: 'SG_INVENTORY',
+  }
+
+};
